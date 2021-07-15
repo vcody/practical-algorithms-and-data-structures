4d6782d16b0cf197431d97a230721d96aae1461d
vcody <codyyv@protonmail.com>
2021-07-15 14:10:51 -0700

    Added hot potato simulation with deque

---
+++ hot_potato.py
@@ -0,0 +1,18 @@
+from collections import deque
+
+def hot_potato(names, num):
+    queue = deque()
+
+    for name in names:
+        queue.appendleft(name)
+    
+    while len(queue) > 1:
+        for _ in range(num):
+            queue.appendleft(queue.pop())
+        
+        queue.pop()
+    
+    return queue.pop()
+
+people = ['Cody', 'Donna', 'Betty', 'Kelley', 'Chris', 'Shareca']
+print(hot_potato(people, 9)) # => 'Donna'
